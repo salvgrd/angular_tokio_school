64410ec61a812f629fc9717bc6b757ac1798e77e
salvgrd <salvgrd@gmail.com>
2021-03-03 02:11:23 +0100

    [feat] init login-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
